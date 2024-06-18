a37fda650a180924a49837de8902045f0b38c485
okomenama <amanetf0323@gmail.com@gmail.com>
2024-06-18 17:14:42 +0900

    6/18

--- README.md
+++ README.md
@@ -1,12 +1,31 @@
 # predictability_tipping
 Folders
 
-Predictability_Tipping_Lab
+.
+├── tipping_element
+│   ├── amazon_tip_num_obs.py
+│   └── amazon_tip_num_non_obs.py
+│   ├── AMOC_tip_num_obs.py
+│   └── AMOC_tip_num_non_obs.py
+│   ├── amazon.py
+│   └── AMOC.py
+│   ├── make_hist.py
+│   └── make_hist_2.py
+│   ├── s_n_plot.py
+│   ├── temp_scenario.py
+├── particle_filter
+│   ├── particle.py
+│   └── __init__.py
+├── data
+|    ├── final_result : In this folder, the results of experiment1 and 2 are included. This results are made by amazon_tip_num_obs.py, amazon_tip_num_non_obs.py,amoc_tip_num__obs.py and amoc_tip_num_non_obs.py. The figures of the results are in /output/amazon or /output/amoc. 
+|       └── amazon
+|       └── amoc
+├── output
+|   |── amazon
+|   |    └── example : Results of amazon.py to show the examples of data assimilation.
+|   |    └── rms : The 30-year average internal variability data and figures. Tempearture scenarios are also in this directory.
+    ├── amoc : The same to ../amazon
+    └── final_result
+         └── amazon
+         ├── amoc
 
---particle_filter
-
---tipping_element
-
---output
-
---data
--- tipping_element/s_n_plot.py
+++ tipping_element/s_n_plot.py
@@ -7,12 +7,16 @@ ax=fig.add_subplot()
 result=[]
 for amp in [1,2,3,4]:
     data_freq='../data/final_result/amazon/tip_num_freq_amp{}.csv'.format(amp)
-    data_sn='../data/final_result/amazon/signal_ratio.csv'
+    #data_sn='../data/final_result/amazon/signal_ratio.csv'
+    data_sig='../output/amazon/rms/np_temp_amp{}.csv'.format(amp)
 
     df_freq=pd.read_csv(data_freq,header=None)
-    df_sn=pd.read_csv(data_sn,header=None)
+    #df_sn=pd.read_csv(data_sn,header=None)
+    df_sig=pd.read_csv(data_sig,header=None)
 
-    arr_sn=df_sn.iloc[0,:].values
+    #arr_sn=df_sn.iloc[0,:].values
+    arr_sig=df_sig.iloc[:,0].values
+    print(arr_sig)
     df_freq=df_freq[df_freq.iloc[:,0]==1]
 
     ##観測の数が100だと1,90だと201,80だと401,70だと601を返すような関数を考える
@@ -23,7 +27,8 @@ for amp in [1,2,3,4]:
         group_obs_num=group.iat[0,2]
         group_noise=group.iat[0,1]
 
-        group_sn=amp/group_noise/abs(arr_sn[0:(-20)*(100-group_obs_num)-1].mean())*0.1**0.5
+        #group_sn=amp/group_noise/abs(arr_sn[0:(-20)*(100-group_obs_num)-1].mean())*0.1**0.5
+        group_sn=arr_sig[0:(-20)*(100-group_obs_num)-1].max()/group_noise
         group_ave=group.iloc[:,3].mean()/1000
         group_var=group.iloc[:,3].var()/1000000
         result.append([group_sn,group_ave,group_var])
@@ -36,7 +41,7 @@ plt.errorbar(result[:,0],result[:,1],np.sqrt(result[:,2]),capsize=5, fmt='o', ma
 plt.title('TRIFFID S/N-Tipping probability')
 plt.xlabel('S/N')
 plt.ylabel('Probability')
-plt.savefig('../output/final_result/amazon/sn_prob_mean.png')
+plt.savefig('../output/final_result/amazon/sn_prob_mean_m.png')
 plt.clf()
 
 
@@ -45,13 +50,16 @@ plt.figure()
 result=[]
 for amp in [1,2,3,4]:
     data_freq='../data/final_result/amoc/tip_num_freq_amp{}.csv'.format(amp)
-    data_sn='../data/final_result/amoc/signal_ratio.csv'
+    #data_sn='../data/final_result/amoc/signal_ratio.csv'
+    data_sig='../output/amoc/rms/np_temp_amp{}.csv'.format(amp)
 
     df_freq=pd.read_csv(data_freq,header=None)
-    df_sn=pd.read_csv(data_sn,header=None)
+    #df_sn=pd.read_csv(data_sn,header=None)
+    df_sig=pd.read_csv(data_sig,header=None)
 
-    arr_sn=df_sn.iloc[0,:].values
-    print(arr_sn)
+    #arr_sn=df_sn.iloc[0,:].values
+    arr_sig=df_sig.iloc[:,0].values
+    #print(arr_sn)
 
     df_freq=df_freq[df_freq.iloc[:,0]==1]
 
@@ -62,18 +70,18 @@ for amp in [1,2,3,4]:
         group_obs_num=group.iat[0,2]
         group_noise=group.iat[0,1]*10
 
-        group_sn=amp/group_noise/abs(arr_sn[2000:(-20)*(100-group_obs_num)-1].mean())*0.1**0.5
+        #group_sn=amp/group_noise/abs(arr_sn[2000:(-20)*(100-group_obs_num)-1].mean())*0.1**0.5
+        group_sn=arr_sig[0:(-20)*(100-group_obs_num)-1].max()/group_noise
         group_ave=group.iloc[:,3].mean()/1000
         group_var=group.iloc[:,3].var()/1000000
         result.append([group_sn,group_ave,group_var])
 
 result=np.array(result)
-plt.xlim(0,10)
 plt.ylim(0.3,1.05)
 plt.errorbar(result[:,0],result[:,1],np.sqrt(result[:,2]),capsize=5, fmt='o', markersize=10, ecolor='black', markeredgecolor = "black", color='w')
 plt.title('AMOC S/N-Tipping probability')
 plt.xlabel('S/N')
 plt.ylabel('Probability')
-plt.savefig('../output/final_result/amoc/sn_prob_mean_l.png')
+plt.savefig('../output/final_result/amoc/sn_prob_mean_m.png')
 plt.clf()   
 
